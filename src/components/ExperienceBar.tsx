55e36cb44d4adc7536263bec2ec911d54c4dfcfe
Lucas <lucaspdroz@gmail.com>
2021-02-28 02:56:40 -0300

    ultimo dia NLW 🎉

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
-import styles from '../styles/components/Experiencebar.module.css';
+import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
 
